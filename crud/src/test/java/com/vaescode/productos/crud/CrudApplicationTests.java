4f000d8060d49a37816a2a2328157b58c327de39
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-09-19 15:06:10 -0500

    Inicializando proyecto REST API - Spring Boot - MongoDB

---
+++ crud/src/test/java/com/vaescode/productos/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaescode.productos.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
